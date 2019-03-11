87a104d31fbd0739d6d2bf51529f8dfafb7873b1
erichui <erichui0906@163.com>
2019-03-11 19:13:22 +0800

    add page

--- routes/entries.js
+++ routes/entries.js
@@ -3,7 +3,11 @@ const router = express.Router()
 const Entry = require('../lib/entry')
 
 router.get('/', (req, res, next) => {
-  Entry.getRange(0, -1, (err, entries) => {
+  const {
+    from,
+    to
+  } = req.page || {}
+  Entry.getRange(from, to, (err, entries) => {
     if(err) return next(err)
     res.render('entries', {
       title: 'entries',
