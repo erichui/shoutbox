5b32bd1da5f6cd431ba49e3549d8c27332ba22fe
erichui <erichui0906@163.com>
2019-03-12 19:34:07 +0800

    page done

--- routes/entries.js
+++ routes/entries.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const Entry = require('../lib/entry')
+const page = require('../lib/middleware/page')
 
-router.get('/', (req, res, next) => {
-  const {
-    from,
-    to
-  } = req.page || {}
-  Entry.getRange(from, to, (err, entries) => {
-    if(err) return next(err)
-    res.render('entries', {
-      title: 'entries',
-      entries
+router.get('/',
+  page(Entry.count, 5),
+  (req, res, next) => {
+    const {
+      from = 0,
+      to = -1
+    } = req.page || {}
+    Entry.getRange(from, to, (err, entries) => {
+      if(err) return next(err)
+      res.render('entries', {
+        title: 'entries',
+        entries
+      })
     })
-  })
-})
+  }
+)
 
 module.exports = router
